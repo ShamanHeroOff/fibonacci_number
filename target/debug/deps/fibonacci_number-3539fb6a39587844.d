c31defc42edc9eaaa311234b682efa9616378a42
agagagaig@mail.ru <agagagaig@mail.ru>
2022-03-11 17:26:45 +0300

    Init commit

--- target/debug/deps/fibonacci_number-3539fb6a39587844.d
+++
@@ -1,5 +0,0 @@
-/home/thx/IdeaProjects/готовые задачи/fibonacci_number+/target/debug/deps/fibonacci_number-3539fb6a39587844: src/main.rs
-
-/home/thx/IdeaProjects/готовые задачи/fibonacci_number+/target/debug/deps/fibonacci_number-3539fb6a39587844.d: src/main.rs
-
-src/main.rs:
--- target/debug/fibonacci_number.d
+++
@@ -1,1 +0,0 @@
-/home/thx/IdeaProjects/готовые\ задачи/fibonacci_number+/target/debug/fibonacci_number: /home/thx/IdeaProjects/готовые\ задачи/fibonacci_number+/src/main.rs
